9f5037d8fda19e16666c101c8abce3679cc94ab6
syauqinzul <nuzulabdisyauqi@gmail.com>
2026-06-06 00:30:09 +0800

    Tipe Data

---
+++ Tipe Data/Tipe Data.js
@@ -0,0 +1,47 @@
+//Tipe Data
+
+// 1 String
+// String digunakan untuk menyimpan data berupa teks, string ditulis dengan menggunakan tanda kutip ("") atau tanda kutip tunggal ('')
+//contoh string
+let nama = "Syauqi Nuzul Abdi"; //string, menggunakan tanda kutip ganda (") karena string ini mengandung tanda kutip tunggal (') di dalamnya
+let namaLengkap = 'Syauqi Nuzul Abdi'; //string, menggunakan tanda kutip tunggal (') karena string ini mengandung tanda kutip ganda (") di dalamnya
+
+// 2 Number
+// Number digunakan untuk menyimpan data berupa angka, number ditulis tanpa menggunakan tanda kutip
+//contoh number
+let usia = 20; //number, menggunakan angka tanpa tanda kutip
+let tinggiBadan = 170.5; //number, menggunakan angka dengan desimal (double float)
+let beratBadan = 60.2; //number, menggunakan angka dengan desimal (double float)
+
+// 3 Boolean
+// Boolean digunakan untuk menyimpan data berupa nilai true atau false, boolean ditulis tanpa menggunakan tanda kutip
+//contoh boolean
+let isStudent = true; //boolean, menggunakan nilai true tanpa tanda kutip
+let isMarried = false; //boolean, menggunakan nilai false tanpa tanda kutip
+
+// 4 Null
+// Null digunakan untuk menyimpan data yang tidak memiliki nilai, null ditulis tanpa menggunakan tanda kutip
+//contoh null
+let pacar = null; //null, menggunakan nilai null tanpa tanda kutip karena belum memiliki pacar
+
+// 5 Undefined
+// Undefined digunakan untuk menyimpan data yang belum memiliki nilai, undefined ditulis tanpa menggunakan tanda kutip
+//contoh undefined
+let pekerjaan; //undefined, karena belum memiliki nilai
+
+// 6 Array
+// Array digunakan untuk menyimpan data yang memiliki banyak nilai, array ditulis dengan menggunakan tanda kurung siku ([]) dan setiap nilai dipisahkan dengan koma (,)
+//contoh array
+let hobi = ["membaca", "bermain game", "berolahraga"]; //array, menggunakan tanda kurung siku ([]) karena kita memiliki banyak hobi, setiap hobi dipisahkan dengan koma (,)
+let namaGuru = ["Pak Budi", "Bu Siti", "Pak Andi"]; //array, menggunakan tanda kurung siku ([]) karena kita memiliki banyak nama guru, setiap nama guru dipisahkan dengan koma (,)
+let namaSiswa = ["Adi", "Budi", "Cici"]; //array, menggunakan tanda kurung siku ([]) karena kita memiliki banyak nama siswa, setiap nama siswa dipisahkan dengan koma (,)
+
+// 7 Object
+// Object digunakan untuk menyimpan data yang memiliki banyak nilai dengan key dan value, object ditulis dengan menggunakan tanda kurung kurawal ({}) dan setiap key dan value dipisahkan dengan koma (,)
+//contoh object
+let alamat = { //object, menggunakan tanda kurung kurawal ({}) karena kita memiliki banyak data alamat, setiap data alamat dipisahkan dengan koma (,)
+  jalan: "Jl. Kapten Piere Tendean, Blok F No 6", //key jalan dengan value "Jl. Kapten Piere Tendean, Blok F No 6"
+  kota: "Jakarta", //key kota dengan value "Jakarta"
+  negara: "Indonesia", //key negara dengan value "Indonesia"
+};
+
