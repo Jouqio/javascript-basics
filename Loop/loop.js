f200bfc05410753863eb153b21f14b75ddb9502a
syauqinzul <nuzulabdisyauqi@gmail.com>
2026-06-05 23:54:20 +0800

    Array

---
+++ Loop/loop.js
@@ -0,0 +1,12 @@
+// for loop
+// for loop digunakan untuk melakukan perulangan dengan jumlah yang sudah ditentukan, for loop terdiri dari 3 bagian yaitu inisialisasi, kondisi, dan increment/decrement
+// inisialisasi digunakan untuk memberikan nilai awal pada variabel yang akan digunakan dalam perulangan, inisialisasi hanya akan dijalankan sekali pada awal perulangan
+// kondisi digunakan untuk memberikan kondisi pada perulangan, perulangan akan terus berjalan selama kondisi bernilai true, jika kondisi bernilai false maka perulangan akan berhenti
+// increment/decrement digunakan untuk memberikan nilai tambah atau kurang pada variabel yang digunakan dalam perulangan, increment/decrement akan dijalankan setiap kali perulangan selesai
+
+//contoh for loop
+for (let i = 0; i < 5; i++) {
+  //inisialisasi i dengan nilai 0, kondisi i kurang dari 5, increment i dengan nilai 1
+  console.log(i); //menampilkan nilai i pada console, nilai i akan berubah setiap kali perulangan selesai
+}
+
