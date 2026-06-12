aaadb97c1dbbd7ec6adfeeb8fc46e136e6ffd6ba
syauqinzul <nuzulabdisyauqi@gmail.com>
2026-06-12 22:59:06 +0800

    tugas

---
+++ Conditional Statement/Tugas/main.js
@@ -0,0 +1,6 @@
+// tugas Statement
+
+function myFunction() {
+  document.getElementById("demo1").innerHTML = "Hello Syauqi Handsome!";
+  document.getElementById("demo2").innerHTML = "How are you?";
+}
