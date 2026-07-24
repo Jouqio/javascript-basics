7231e29b7a6d3745f8b20d43b1c3a28382a45c99
syauqinzul <nuzulabdisyauqi@gmail.com>
2026-07-25 02:26:11 +0800

    Comment

--- Comment/main.js
+++ Comment/main.js
@@ -6,3 +6,18 @@ console.log("Hai, JavaScript!");
 // Jika Anda jalankan kode di atas, hanya akan tampil teks "Hai, Readers!” dan
 // “Hai, JavaScript!”. Kode yang ditulis pada baris pertama dan terakhir akan
 // diabaikan karena dianggap sebagai komentar.
+
+//Berikut adalah contoh dari komentar yang ditulis dengan tanda /* */. 
+/* 
+Ini adalah komentar multiline
+yang dapat digunakan untuk menjelaskan kode yang kompleks.
+*/
+
+/*
+ * TODO
+ * 1. Buatlah variabel bernama `PI` dan isikan dengan nilai 3.14
+ * 2. Cetak nilai variabel PI di terminal menggunakan console.log
+ */
+
+const PI = '3.14';
+console.log(PI);
